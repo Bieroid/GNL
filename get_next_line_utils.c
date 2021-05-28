f00fcb7ca5d1fd3a4db7f1767aaf99bf0e68aee6
Humma Tommy <htommy@mi-b5.msk.21-school.ru>
2021-05-28 20:15:36 +0300

    2

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -11,11 +11,14 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 size_t	ft_strlen(const char *s)
 {
 	int	a;
 
+	if (s == NULL)
+		return (0);
 	a = 0;
 	while (s[a] != '\0')
 		a++;
@@ -41,20 +44,18 @@ void	*ft_calloc(size_t num, size_t size)
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		a;
-	int		b;
-	int		c;
-	char	*s;
+	size_t 		a;
+	size_t 		b;
+	size_t		c;
+	char		*s;
 
-	if ((s1 == 0) || (s2 == 0))
-		return (0);
 	c = -1;
 	a = ft_strlen(s1);
 	b = ft_strlen(s2);
 	s = ft_calloc(sizeof(char), (a + b) + 1);
 	if (s == 0)
 		return (0);
-	while (++c <= a)
+	while (++c < a)
 		s[c] = s1[c];
 	c--;
 	a = 0;
@@ -65,4 +66,78 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		c++;
 	}
 	return (s);
+}
+
+static char 	*ft_socr1(size_t len, char const *s, unsigned int start)
+{
+	size_t	a;
+	char	*sub;
+
+	a = 0;
+	sub = ft_calloc(sizeof(char), len + 1);
+	if (sub == 0)
+		return (0);
+	while (a < len)
+	{
+		sub[a] = s[start];
+		a++;
+		start++;
+	}
+	return (sub);
+}
+
+static char 	*ft_socr2(size_t a, unsigned int start, char const *s)
+{
+	char	*sub;
+
+	sub = ft_calloc(sizeof(char), (a - start) + 1);
+	if (sub == 0)
+		return (0);
+	a = 0;
+	while (s[start] != '\0')
+	{
+		sub[a] = s[start];
+		a++;
+		start++;
+	}
+	return (sub);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	a;
+	char	*sub;
+
+	if (s == 0)
+		return (0);
+	a = ft_strlen(s);
+	if (start > a)
+	{
+		sub = ft_calloc(sizeof(char), 1);
+		if (sub == 0)
+			return (0);
+		return (sub);
+	}
+	if (a - start > len)
+		sub = ft_socr1(len, s, start);
+	else
+		sub = ft_socr2(a, start, s);
+	return (sub);
+}
+
+char	*ft_strchr (const char *str, int ch)
+{
+	int	a;
+
+	ch = ch % 256;
+	a = 0;
+	while (str[a] != '\0')
+	{
+		if (str[a] == ch)
+			return ((char *)&str[a]);
+		a++;
+	}
+	if (str[a] == ch)
+		return ((char *)&str[a]);
+	return (0);
 }
